fa51e92f9eb45c224175368321131c13a089f4fa
Benedikt Klein <benediktklein@MBPvonBenedikt.fritz.box>
2025-11-06 21:22:54 +0100

    Add homepage to fix 404

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // wichtig für Vercel
+  distDir: ".next",     // Standard-Build-Verzeichnis
+  reactStrictMode: true,
 };
 
 export default nextConfig;
