e75e05592596677b06eeb6ff71a36c2dd7fdebdd
Benedikt Klein <benediktklein@MBPvonBenedikt.fritz.box>
2025-11-07 10:45:29 +0100

    Add homepage to fix 404

--- app/clubs/ClubMap.tsx
+++ app/clubs/ClubMap.tsx
@@ -3,7 +3,6 @@
 import dynamic from "next/dynamic";
 import L from "leaflet";
 import React from "react";
-
 import "leaflet/dist/leaflet.css";
 
 // Leaflet Marker fix
@@ -14,13 +13,39 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
-// Dynamischer Import von React-Leaflet
-const MapContainer = dynamic(() => import("react-leaflet").then(m => m.MapContainer), { ssr: false });
-const TileLayer = dynamic(() => import("react-leaflet").then(m => m.TileLayer), { ssr: false });
-const Marker = dynamic(() => import("react-leaflet").then(m => m.Marker), { ssr: false });
-const Popup = dynamic(() => import("react-leaflet").then(m => m.Popup), { ssr: false });
+// Dynamischer Import der gesamten Map-Komponente, nur client-side
+const LeafletMap = dynamic(
+  async () => {
+    const { MapContainer, TileLayer, Marker, Popup } = await import("react-leaflet");
+    return function Map({ clubs }: { clubs: Club[] }) {
+      const first = clubs.find(c => c.lat && c.lon)!;
+      return (
+        <MapContainer
+          style={{ height: "400px", width: "100%", marginTop: "20px", borderRadius: "12px" }}
+          center={[first.lat!, first.lon!] as [number, number]}
+          zoom={4}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {clubs.map(c => c.lat && c.lon && (
+            <Marker key={c.id} position={[c.lat!, c.lon!] as [number, number]}>
+              <Popup>
+                <strong>{c.name}</strong><br />
+                {c.city}, {c.country}<br />
+                Rating: {c.rating?.toFixed(1)}
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      );
+    };
+  },
+  { ssr: false }
+);
 
-// Typdefinition lokal
 export interface Club {
   id: number;
   name: string;
@@ -38,29 +63,5 @@ interface ClubMapProps {
 
 export default function ClubMap({ clubs }: ClubMapProps) {
   if (!clubs.length || !clubs.some(c => c.lat && c.lon)) return null;
-
-  const firstClubWithCoords = clubs.find(c => c.lat && c.lon)!;
-
-  return (
-    <MapContainer
-      style={{ height: "400px", width: "100%", marginTop: "20px", borderRadius: "12px" }}
-      center={[firstClubWithCoords.lat!, firstClubWithCoords.lon!] as [number, number]}
-      zoom={4}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {clubs.map(club => club.lat && club.lon && (
-        <Marker key={club.id} position={[club.lat, club.lon] as [number, number]}>
-          <Popup>
-            <strong>{club.name}</strong><br />
-            {club.city}, {club.country}<br />
-            Rating: {club.rating?.toFixed(1)}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+  return <LeafletMap clubs={clubs} />;
 }
