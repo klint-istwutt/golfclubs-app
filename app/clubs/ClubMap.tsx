7ff0495a924f62272b6a03b0d41610cbfd2fda55
Benedikt Klein <benediktklein@MBPvonBenedikt.fritz.box>
2025-11-07 22:59:49 +0100

    Add homepage to fix 404

--- app/clubs/ClubMap.tsx
+++ app/clubs/ClubMap.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import type { LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect } from "react";
@@ -21,9 +21,49 @@ interface ClubMapProps {
   mapCenter: LatLngExpression;
 }
 
+// 🧭 Steuerungskomponente (separat, um Map-Instanz zu bekommen)
+function MapControl() {
+  const map = useMap();
+
+  useEffect(() => {
+    // Standardmäßig: Kein Scroll-Zoom
+    map.scrollWheelZoom.disable();
+
+    const container = map.getContainer();
+
+    const enableCtrlZoom = (e: KeyboardEvent) => {
+      if (e.key === "Control") {
+        map.scrollWheelZoom.enable();
+        container.classList.add("ctrl-zoom-active");
+      }
+    };
+
+    const disableCtrlZoom = (e: KeyboardEvent) => {
+      if (e.key === "Control") {
+        map.scrollWheelZoom.disable();
+        container.classList.remove("ctrl-zoom-active");
+      }
+    };
+
+    // Zwei-Finger-Zoom bleibt aktiv für Touchgeräte
+    map.touchZoom.enable();
+    map.doubleClickZoom.disable();
+
+    document.addEventListener("keydown", enableCtrlZoom);
+    document.addEventListener("keyup", disableCtrlZoom);
+
+    return () => {
+      document.removeEventListener("keydown", enableCtrlZoom);
+      document.removeEventListener("keyup", disableCtrlZoom);
+    };
+  }, [map]);
+
+  return null;
+}
+
 export default function ClubMap({ clubs, mapCenter }: ClubMapProps) {
   useEffect(() => {
-    // Leaflet Standard-Marker setzen
+    // Leaflet Marker fixen (Pfadproblem vermeiden)
     L.Icon.Default.mergeOptions({
       iconRetinaUrl:
         "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
@@ -36,24 +76,42 @@ export default function ClubMap({ clubs, mapCenter }: ClubMapProps) {
 
   return (
     <MapContainer
-      style={{ height: 400, width: "100%", marginTop: 20, borderRadius: 12 }}
+      style={{
+        height: 400,
+        width: "100%",
+        marginTop: 20,
+        borderRadius: 12,
+        zIndex: 0,
+      }}
       center={mapCenter}
       zoom={4}
-      scrollWheelZoom={true}
+      scrollWheelZoom={false} // Initial deaktiviert
     >
+      {/* Tile Layer */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {clubs.map(c => c.lat && c.lon && (
-        <Marker key={c.id} position={[c.lat, c.lon]}>
-          <Popup>
-            <strong>{c.name}</strong><br />
-            {c.city}, {c.country}<br />
-            Rating: {c.rating?.toFixed(1)}
-          </Popup>
-        </Marker>
-      ))}
+
+      {/* STRG-/Touch-Steuerung aktivieren */}
+      <MapControl />
+
+      {/* Marker */}
+      {clubs.map(
+        (c) =>
+          c.lat &&
+          c.lon && (
+            <Marker key={c.id} position={[c.lat, c.lon]}>
+              <Popup>
+                <strong>{c.name}</strong>
+                <br />
+                {c.city}, {c.country}
+                <br />
+                Rating: {c.rating?.toFixed(1)}
+              </Popup>
+            </Marker>
+          )
+      )}
     </MapContainer>
   );
 }
